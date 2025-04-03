4acf6e209eb940b4296d564dc4c7375e508f0024
Chaitanya Nanduri <chaitanyananduri@Spotless-Mind.local>
2025-04-03 19:20:27 +0530

    commit

--- trail_proj1.py
+++ trail_proj1.py
@@ -1,5 +1,5 @@
 import pandas as pd
 import numpy as np
 # import os
-
+print("Pandas Version is ", pd.__version__ , "and NumPy Version is ", np.__version__)
 print("First line of code in python local")
